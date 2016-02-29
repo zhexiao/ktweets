7947cb1df63e6cf7c92a8cc7d47eec8acc288e33
zhexiao <zhexiao@163.com>
2016-02-29 14:00:54 -0500

    css

--- kweets/urls.py
+++ kweets/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from tweets import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index),
     url(r'^tweets/', include('tweets.urls', namespace='tweets', app_name='tweets')),
-    url(r'^account/', include('account.urls', namespace='account', app_name='account'))
+    url(r'^account/', include('account.urls', namespace='account', app_name='account')),
 ]
