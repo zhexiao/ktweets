c0370d805d9be5d5da095a4e45e43fb3f3d7eb79
zhexiao <zhexiao@163.com>
2016-04-06 00:30:23 +0000

    add tests

--- account/tests.py
+++ account/tests.py
@@ -2,7 +2,11 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 
 class UserModelTests(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.user = User.objects.create_user(username='zx', email='zx@gmail.com', password='zx')
+
     def test_user_email_unique(self):
-        acct = User.objects.get(email="zhexiao27@gmail.com")
-        self.assertEqual(acct.id, 1)
+        acct = User.objects.get(email="zx@gmail.com")
+        self.assertEqual(acct.username, self.user.username)
 
